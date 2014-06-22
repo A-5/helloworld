b45d34dcc4078be894d8f3ad13fb541d6f976b10
A-5 <after5bya5@gmail.com>
2014-06-22 23:59:50 +0900

    gitcomit

---
+++ uby hello.rb
@@ -0,0 +1,15 @@
+# コメントは半角＃だよ
+#/* ブロックコメントはいける？ ブロックコメントは駄目だよ*/
+# 文字を表示させるよ
+puts 'Hello, world!';
+# Cのようにもかけるよ。
+a = 1;
+printf("Hello, world! a = \"%d\"\n",a);
+
+#条件分岐は
+if 1 == a then
+	#printf("あ\n");
+else
+	printf("YYY\n");
+end
+
